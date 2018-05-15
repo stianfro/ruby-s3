af08b191eeba537fba9338a446fa2c676f48fe79
Stian Frøystein <stianfroy@gmail.com>
2018-05-15 13:20:03 +0200

    initial commit

---
+++ s3.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+require 'aws-sdk'
+
+endpoint = ENV['S3_ENDPOINT']
+
+credentials = Aws::SharedCredentials.new(profile_name: 'default')
+
+client = Aws::S3::Client.new(
+	region: 'us-east-1',
+	endpoint: endpoint,
+	credentials: credentials,
+	force_path_style: true
+)
+
+s3 = Aws::S3::Resource.new(client: client)
+
+# s3.bucket('hello-bucket').objects.each.delete
+
+a = 00001
+
+while true
+	a += 1
+	object = s3.bucket('hello-bucket').object("hello-object-#{a}")
+
+	object.put(
+		metadata: {
+			'hello' => 'world'
+		},
+		body: 'Hello World!',
+		server_side_encryption: 'AES256'
+	)
+	puts "Object key: #{object.key}"
+	sleep 0.3
+end
