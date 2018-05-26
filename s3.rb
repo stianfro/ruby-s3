d5146f520cb828cbd0c122a550cd869e9750ed1f
Stian Frøystein <stianfroy@gmail.com>
2018-05-26 16:54:57 +0200

    Update s3.rb

--- s3.rb
+++ s3.rb
@@ -7,10 +7,10 @@ endpoint = ENV['S3_ENDPOINT']
 credentials = Aws::SharedCredentials.new(profile_name: 'default', path: '/usr/src/app/.aws/credentials')
 
 client = Aws::S3::Client.new(
-	region: 'us-east-1',
-	endpoint: endpoint,
-	credentials: credentials,
-	force_path_style: true
+  region: 'us-east-1',
+  endpoint: endpoint,
+  credentials: credentials,
+  force_path_style: true
 )
 
 s3 = Aws::S3::Resource.new(client: client)
@@ -18,10 +18,10 @@ s3 = Aws::S3::Resource.new(client: client)
 a = 1
 
 while true
-	a += 1
-	object = s3.bucket('hello-bucket').object("hello-object-#{a}")
+  a += 1
+  object = s3.bucket('hello-bucket').object("hello-object-#{a}")
 
-	object.upload_file('/usr/src/app/test_file')
+  object.upload_file('/usr/src/app/test_file')
 
-	puts "Object key: #{object.key}, size: #{object.size}, number: #{a}"
+  puts "Object key: #{object.key}, size: #{object.size}, number: #{a}"
 end
